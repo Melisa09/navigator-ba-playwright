2e5ba6f20c8b3b466fc36ab431ff0c0add591751
Melisa09 <melisa.melka@hotmail.com>
2025-04-29 16:38:16 +0200

    smoke test cases and test

---
+++ README.md
@@ -0,0 +1,29 @@
+# Navigator.ba Test Automation
+
+## Description
+This project automates the testing of the Navigator.ba website using Playwright and Python for Atlantbh assignment. The purpose is to ensure that all basic functionalities of the website work correctly, including navigation, search and map.
+
+## Test Cases
+The test cases for the Navigator.ba application are documented in the [test_cases.xlsx](./test_cases.xlsx) file. You can review all the test scenarios in this file.
+
+## Bugs
+The known bugs for the Navigator.ba application are documented in the [bugs.xlsx](./bugs.xlsx). You can review the details of the reported bugs, their descriptions, and current status in this file.
+
+## How to Set Up and Run Tests
+
+1. Clone the repository:
+    git clone https://github.com/Melisa09/navigator-ba-playwright.git
+    cd your-repository
+
+2. Set up the virtual environment:
+    python -m venv venv
+    venv\Scripts\activate  #windows
+   
+3. Install dependencies:
+    pip install -r requirements.txt
+
+4. Run the smoke test:
+    python test_navigator_smoke.py 
+
+## Contact
+For any inquiries, please contact melisa.herovic@gmail.com
--- test_navigator_smoke.py
+++ test_navigator_smoke.py
@@ -1,13 +1,88 @@
+import time
 from playwright.sync_api import sync_playwright
 
-def test_navigator_homepage():
+def test_nb16_valid_search(page):
+    page.goto("https://www.navigator.ba/#/categories")
+    page.wait_for_load_state('load')
+    textbox = page.get_by_role("textbox", name="Traži ulicu ili objekat")
+    textbox.click()
+    textbox.press("CapsLock")
+    textbox.fill("H")
+    textbox.press("CapsLock")
+    textbox.fill("Hrasno")
+    textbox.press("Enter")
+    time.sleep(5)
+
+def test_nb17_invalid_search(page):
+    page.goto("https://www.navigator.ba/#/categories")
+    page.wait_for_load_state('load')
+    textbox = page.get_by_role("textbox", name="Traži ulicu ili objekat")
+    textbox.click()
+    textbox.fill("2333")
+    textbox.press("Enter")
+    time.sleep(5)
+    print("There is no posssiblity for searching by numbers.")
+
+def test_nb18_empty_search(page):
+    page.goto("https://www.navigator.ba/#/categories")
+    page.wait_for_load_state('load')
+    page.get_by_role("textbox", name="Traži ulicu ili objekat").click()
+    page.wait_for_timeout(2000)
+    page.get_by_role("textbox", name="Traži ulicu ili objekat").press("Enter")
+    page.wait_for_timeout(2000)
+
+def test_nb19_special_characters_search(page):
+    page.goto("https://www.navigator.ba/#/categories")
+    page.wait_for_load_state('load')
+    page.get_by_role("textbox", name="Traži ulicu ili objekat").click()
+    page.get_by_role("textbox", name="Traži ulicu ili objekat").fill("Franz & Sophie")
+    page.get_by_role("textbox", name="Traži ulicu ili objekat").press("Enter")
+    time.sleep(5)
+
+def test_nb20_autocomplete_suggestions(page):
+    page.goto("https://www.navigator.ba/#/categories")
+    page.wait_for_load_state('load')
+    page.get_by_role("link", name="SARAJEVO").click()
+    page.get_by_role("textbox", name="Traži ulicu ili objekat").click()
+    page.get_by_role("textbox", name="Traži ulicu ili objekat").fill("trave")
+    page.get_by_text("Hostel/Travel Agency \"Ljubič").click()
+    page.wait_for_timeout(2000)
+
+def test_nb21_case_insensitive_search(page):
+
+    page.goto("https://www.navigator.ba/#/categories")
+    page.wait_for_load_state('load')
+    page.get_by_role("textbox", name="Traži ulicu ili objekat").click()
+    page.get_by_role("textbox", name="Traži ulicu ili objekat").fill("hotel vip")
+    page.get_by_role("textbox", name="Traži ulicu ili objekat").press("Enter")
+    page.wait_for_timeout(2000)
+    page.get_by_role("link", name="Hotel VIP Jaroslava Černija 3").click()
+    page.wait_for_timeout(2000)
+
+    page.get_by_role("textbox", name="Traži ulicu ili objekat").click()
+    page.get_by_role("textbox", name="Traži ulicu ili objekat").fill("HOTEL VIP")
+    page.get_by_role("textbox", name="Traži ulicu ili objekat").press("Enter")
+    page.wait_for_timeout(2000)
+    page.get_by_role("link", name="Hotel VIP Jaroslava Černija 3").click()
+    page.wait_for_timeout(2000)
+
+def run_tests():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)  # headless=False = we can see browser
-        page = browser.new_page()
-        page.goto("https://www.navigator.ba")
-        assert "Navigator" in page.title()
-        print("Homepage loaded successfully and title verified!")
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+
+        test_nb16_valid_search(page)
+        test_nb17_invalid_search(page)
+        test_nb18_empty_search(page)
+        test_nb19_special_characters_search(page)
+        test_nb21_case_insensitive_search(page)
+        test_nb20_autocomplete_suggestions(page)
+      
+
+        context.close()
         browser.close()
 
 if __name__ == "__main__":
-    test_navigator_homepage()
+    run_tests()
+
